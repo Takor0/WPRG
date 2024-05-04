fc74733ee8e5eab6478320a3232cc0c3e37e4c1e
tomi0 <tomi0985@wp.pl>
2024-05-04 22:26:30 +0200

    zadanie 1

--- zestaw4/zadanie1.php
+++ zestaw4/zadanie1.php
@@ -1,22 +1,19 @@
-<html>
-<body>
-<form method="POST">
-    <input type="file" name="file">
-    <button type="submit"> prześlij</button>
-</form>
 <?php
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['file'])) {
-        $file = $_POST['file'];
-        while (!feof($file)) {
-            $str = fgets($file);
-            $str = nl2br($str);
-            echo($str);
-        }
+    if (isset($_FILES['user-file'])) {
+        $filename = $_FILES['user-file']['tmp_name'];
+        $file_content = file_get_contents($filename);
+		$lines = explode("\n", $file_content);
+        $lines = array_reverse($lines);
+        file_put_contents($filename, implode("\n", $lines));
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . basename($_FILES['user-file']['name']) . '"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filename));
+        readfile($filename);
     }
 }
-?>
-</body>
-</html>
