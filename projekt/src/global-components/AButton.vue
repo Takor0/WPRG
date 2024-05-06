09f629d4bb602d0855e3b089ff1893b4d167d596
tomi0 <tomi0985@wp.pl>
2024-05-06 23:45:24 +0200

    some things

---
+++ projekt/src/global-components/AButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "AButton",
+})
+</script>
+
+<style scoped lang="scss"></style>
--- projekt/src/main.ts
+++ projekt/src/main.ts
@@ -1,6 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import components from "@/global-components/index"
+// TODO: add global components
+console.log(components)
 
-createApp(App).use(store).use(router).mount("#app");
+import "@fontsource/ubuntu"
+
+createApp(App).use(store).use(router).mount("#app")
